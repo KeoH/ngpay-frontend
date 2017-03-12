1b64f449572b83f138a665bc22fe5dad11e45baa
Francisco Manzano <keoh77@gmail.com>
2017-03-12 19:02:57 +0100

    new payment-list component

---
+++ src/app/payments-list/payments-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngpay-payments-list',
+  templateUrl: './payments-list.component.html',
+  styleUrls: ['./payments-list.component.sass']
+})
+export class PaymentsListComponent {
+
+  constructor() { }
+
+}
