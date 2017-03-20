caf6993d65a371b15bf855bc5947a05d69be0670
Francisco Manzano <keoh77@gmail.com>
2017-03-20 20:03:13 +0100

    Bootstrap 4 sass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { MaterialModule } from '@angular/material';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
@@ -31,7 +30,6 @@ import { LoginFormComponent } from './_components/';
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule,
     ReactiveFormsModule,
     routing
   ],
